3f1b44113ab1daaf1d512af7b4c5a8d70f6d24cb
imshimsin <honeysinoriya@gmail.com>
2026-01-31 01:13:30 +0530

    Register api  middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,30 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'api.auth' => \App\Http\Middleware\AuthenticateByHeader::class,
+            'api.user' => \App\Http\Middleware\EnsureApiUser::class,
+            'passenger' => \App\Http\Middleware\EnsureUserIsPassenger::class,
+            'driver' => \App\Http\Middleware\EnsureUserIsDriver::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Resource not found.',
+                ], 404);
+            }
+        });
     })->create();
